5564099177428fd1218e350ea4cf3d848f745558
shiwirockztar <68036093+shiwirockztar@users.noreply.github.com>
2025-11-20 14:45:38 +0000

    second script
    add pruebas.py frontalface detection script

--- Raspberry/prueba.py
+++ Raspberry/prueba.py
@@ -1,22 +1,17 @@
 import cv2
-
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#cap = cv2.VideoCapture(0)
 url = 'http://192.168.1.86:81/stream' 
 cap = cv2.VideoCapture(url)
-
-WinName = 'ESP32 CAMERA'
-cv2.namedWindow(WinName, cv2.WINDOW_AUTOSIZE)   
-
 while True:
-    cap.open(url)
     ret, frame = cap.read()
-
-    if ret:
-        #frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE) 
-        #gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow(WinName, frame)
-
-    tecla = cv2.waitKey(5) & 0xFF
-    if tecla == 27:
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+                                 
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    cv2.imshow('Face Detection', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+cap.release()
 cv2.destroyAllWindows()
-cap.release()   
